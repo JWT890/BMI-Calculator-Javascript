92e9fcf8e2b1cad4d93e4738a2c2f59cb8dd028b
Jon William Taylor <jwtaylor659@gmail.com>
2023-12-12 09:22:38 -0600

    Testing Tabnine

--- Visual Code Studio BMI Calculator JavaScript/main.test.js
+++ Visual Code Studio BMI Calculator JavaScript/main.test.js
@@ -1,8 +1,60 @@
 const calculate = require('./main');
 
-describe('calculate', () => {
-    test('should calculate BMI and return normal', () => {
-        const result = calculate(50, 170);
-        expect(result).toBe('Your BMI is 24.8 which means you are normal');
-    });
+test('should return an error if the user enters no weight or height', () => {
+
+  const mockWindow = {
+    document: {
+      getElementById: jest.fn().mockImplementation((id) => {
+        if (id === 'weight') {
+          return { value: '' };
+        } else if (id === 'height') {
+          return { value: '' };
+        }
+      }),
+      createElement: jest.fn(),
+      getElementById: jest.fn().mockReturnValue({
+        innerHTML: '',
+      }),
+    },
+  };
+
+  global.window = mockWindow;
+});
+
+test('should return an error if the user enters a negative weight', () => {
+
+  const mockWindow = {
+    document: {
+      getElementById: jest.fn().mockImplementation((id) => {
+        if (id === 'weight') {
+          return { value: '-10' };
+        } else if (id === 'height') {
+          return { value: '170' };
+        }
+      }),
+      createElement: jest.fn(),
+      getElementById: jest.fn().mockReturnValue({
+        innerHTML: '',
+      }),
+    },
+  };
+});
+
+test('should calculate the BMI and return the appropriate message if the user enters a valid weight and height', () => {
+
+  const mockWindow = {
+    document: {
+      getElementById: jest.fn().mockImplementation((id) => {
+        if (id === 'weight') {
+          return { value: '70' };
+        } else if (id === 'height') {
+          return { value: '170' };
+        }
+      }),
+      createElement: jest.fn(),
+      getElementById: jest.fn().mockReturnValue({
+        innerHTML: '',
+      }),
+    },
+  };
 });
